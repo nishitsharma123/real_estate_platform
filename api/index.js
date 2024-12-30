0afe67a0ae7aca34a89557875f11e492f5f50690
nishit sharma <nishitsharma128@gmail.com>
2024-12-30 22:34:40 +0530

    deploy code update

--- api/index.js
+++ api/index.js
@@ -1,46 +1,51 @@
-
-import priceEvaluatorRouter from './routes/priceEvaluator.js';
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth.route.js';
-import cookieParser from 'cookie-parser';
-import listingRouter from './routes/listing.route.js';
-import cors from 'cors';
+import priceEvaluatorRouter from "./routes/priceEvaluator.js";
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
+import listingRouter from "./routes/listing.route.js";
+import cors from "cors";
+import path from "path";
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(()=>{
-    console.log('connected to mongodb!');
-}).catch((err)=>{
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to mongodb!");
+  })
+  .catch((err) => {
     console.log(err);
-});
-const app=express();
+  });
+
+const __dirname = path.resolve();
+
+const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(3000,()=>{
-    console.log('server is running on port 3000\n');
-}
-);
+app.listen(3000, () => {
+  console.log("server is running on port 3000\n");
+});
 
-
-app.use("/api/user",userRouter);
-app.use("/api/auth",authRouter);
-app.use('/api/listing',listingRouter);
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 // Use the route
-app.use('/api', priceEvaluatorRouter);
+app.use("/api", priceEvaluatorRouter);
 
-
-
-
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 app.use((err, req, res, next) => {
-const statusCode = err.statusCode || 500;
-const message = err.message || 'Internal server error';
-return res.status(statusCode).json({
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  return res.status(statusCode).json({
     success: false,
     statusCode,
     message,
-});
+  });
 });
